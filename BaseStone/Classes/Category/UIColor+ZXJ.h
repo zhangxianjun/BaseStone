c6d26f457aba86efeb900036bd8b0fc7d0727795
zhangxianjun <1581758003@qq.com>
2017-05-08 10:23:29 +0800

    Add UIColor category

--- BaseStone/Classes/Category/UIColor+ZXJ.h
+++ BaseStone/Classes/Category/UIColor+ZXJ.h
@@ -10,4 +10,21 @@
 
 @interface UIColor (ZXJ)
 
+/**
+ 输入16进制色值，返回颜色对象
+
+ @param value 16进制色值字符串
+ @return 颜色对象
+ */
++ (UIColor *)zxj_colorWithHexString:(NSString *)value;
+
+
+/**
+ 输入16进制色值，透明度，返回颜色对象
+
+ @param value 16进制色值
+ @param alpha 透明度
+ @return 颜色对象
+ */
++ (UIColor *)zxj_colorWithHexString:(NSString *)value alpha:(float)alpha;
 @end
--- BaseStone/Classes/Category/UIColor+ZXJ.m
+++ BaseStone/Classes/Category/UIColor+ZXJ.m
@@ -1,4 +1,4 @@
-//
+ //
 //  UIColor+ZXJ.m
 //  Pods
 //
@@ -9,5 +9,43 @@
 #import "UIColor+ZXJ.h"
 
 @implementation UIColor (ZXJ)
+
++ (UIColor *)zxj_colorWithHexString:(NSString *)value {
+    return [self zxj_colorWithHexString:value alpha:1.0];
+}
+
++ (UIColor *)zxj_colorWithHexString:(NSString *)value alpha:(float)alpha{
+    NSString *cString = [[value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor whiteColor];//DEFAULT_VOID_COLOR;
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    if ([cString hasPrefix:@"#"]) cString = [cString substringFromIndex:1];
+    if ([cString length] != 6) return [UIColor whiteColor];//DEFAULT_VOID_COLOR;
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:alpha];
+}
 
 @end
